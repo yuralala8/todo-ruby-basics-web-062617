f81540753a61d04abb55431356c03fc947f1376c
yuralala8 <yc1435@nyu.edu>
2017-06-10 17:46:52 -0400

    todo commit

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
- 
+  quotient = num1 / num2git
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
+  puts value
+  value
 end
 
-def argue
-  
+def argue(phrase)
+ puts phrase
+ phrase
 end
 
-def greeting
-
+def greeting(hello, name)
+    puts "#{hello}, #{name}."
 end
 
-def return_a_value
-
+def return_a_value(string = "Nice")
+    puts string
+    string
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(string = "expert")
+    puts string
+    string
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+    puts topping
+    topping
 end
